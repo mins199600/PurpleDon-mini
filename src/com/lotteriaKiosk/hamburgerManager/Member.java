cda96b14e39fcf58c22345b5e141eeb7660350c4
jeonghee <hongi_@naver.com>
2024-01-04 20:01:29 +0900

    first commit

--- src/com/lotteriaKiosk/hamburgerManager/Member.java
+++ src/com/lotteriaKiosk/hamburgerManager/Member.java
@@ -1,4 +1,23 @@
 package com.lotteriaKiosk.hamburgerManager;
 
-public class Member {
+import java.util.Scanner;
+
+
+public class Member{
+    private String name;
+    private int number;
+    private int age;
+
+    //생성자
+    public Member(String name, int number, int age){
+        this.name = name;
+        this.number = number;
+        this.age = age;
+    }
+
 }
+
+
+
+
+
